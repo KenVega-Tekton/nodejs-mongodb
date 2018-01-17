9a95946acb9857062d94d9ec4acabd9621536fba
Ken <ken.vega@tektonlabs.com>
2018-01-17 14:04:05 -0500

    usando la funcion count() para contar los documentos

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -8,11 +8,10 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
 
   db
     .collection("Todos")
-    .find({ _id: new ObjectID("5a5f59547ccef22261d99e77") })
-    .toArray()
-    .then(docs => {
-      console.log("Todos");
-      console.log(JSON.stringify(docs, undefined, 2));
+    .find()
+    .count()
+    .then(count => {
+      console.log("Todos counted : ", count);
     })
     .catch(err => {
       console.log("Unable to fetch todos: ", err);
